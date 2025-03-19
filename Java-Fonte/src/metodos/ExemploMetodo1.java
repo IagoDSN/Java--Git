fbf75395e3cec62d8c9870ba88921f351e1ed9c3
Iago Nunes <155097663+IagoDSN@users.noreply.github.com>
2025-03-19 10:08:50 -0300

    Create metodos
    aula sobre metodos em Java

---
+++ Java-Fonte/src/metodos/ExemploMetodo1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+/**
+ *
+ * @author 13410289682
+ */
+public class ExemploMetodo1 { 
+    public static void mostrarMensagem(){
+        System.out.println("Bon ju");
+    }
+    public static void main(String[] args) {
+        
+    mostrarMensagem();
+    mostrarMensagem();
+    mostrarMensagem();
+    mostrarMensagem();
+    mostrarMensagem();
+    }
+}
