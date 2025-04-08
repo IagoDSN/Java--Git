729c09a12a242f1dd29bce6d41c0d7bbd9f96d94
Iago Nunes <155097663+IagoDSN@users.noreply.github.com>
2025-04-07 22:29:12 -0300

    att  02-04

--- Java-Fonte/src/Atividade02_04/Aluno.java
+++ Java-Fonte/src/Atividade02_04/Aluno.java
@@ -5,36 +5,24 @@
  */
 package Atividade02_04;
 
-import java.util.Scanner;
-
 /**
  *
  * @author 13410289682
  */
 public class Aluno {
-     Scanner entrada = new Scanner(System.in);
      
     String nome;
     int matricula, idade;
     
-    void exibirDados(){
+
+    public void fazerMatricula(){
+        System.out.println("----------------------------------");
+        System.out.println(nome + " foi matriculado com sucesso!");
+        System.out.println("----------------------------------");
+    }
+    public void exibirDados() {       
         System.out.println("Nome do aluno: "+this.nome);
         System.out.println("Matricula do aluno: "+this.matricula);
         System.out.println("Idade do aluno: "+this.idade);
     }
-    void fazerMatricula(String nome, int matricula,int idade){
-        System.out.println("Fazer matricula do aluno");
-        System.out.println("----------------------------------");
-        
-        System.out.println("Digite o nome do aluno");
-        this.nome = nome;
-        
-        System.out.println("Digite o nome do aluno");
-        this.matricula = matricula;
-        
-        System.out.println("Digite o nome do aluno");
-        this.idade = idade;
-        
-        System.out.println("---------------------------------");
-}
 }
--- Java-Fonte/src/Atividade02_04/TesteAluno.java
+++ Java-Fonte/src/Atividade02_04/TesteAluno.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package Atividade02_04;
-
 import java.util.Scanner;
 
 /**
@@ -13,17 +12,30 @@ import java.util.Scanner;
  */
 public class TesteAluno {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
         Aluno alu1 = new Aluno();
-        Aluno alu2 = new Aluno();
-        
-                  
-        alu1.fazerMatricula();                  
-        alu2.fazerMatricula();
-        
+        System.out.println("Digite o nome do primeiro aluno:");
+        alu1.nome = scanner.nextLine();
+        System.out.println("Digite a matrícula do primeiro aluno:");
+        alu1.matricula = scanner.nextInt();
+        System.out.println("Digite a idade do primeiro aluno:");
+        alu1.idade = scanner.nextInt();
+        scanner.nextLine();
+        alu1.fazerMatricula();
         alu1.exibirDados();
+        
+        Aluno alu2 = new Aluno();
+         System.out.println("Digite o nome do segundo aluno:");
+        alu2.nome = scanner.nextLine();
+        System.out.println("Digite a matrícula do segundo aluno:");
+        alu2.matricula = scanner.nextInt();
+        System.out.println("Digite a idade do segundp aluno:");
+        alu2.idade = scanner.nextInt();
+        scanner.nextLine();
+        alu2.fazerMatricula();
         alu2.exibirDados();
+                  
         
     }
 }
