662ae390fcbc9b94ac69cb9771717a7f7feee471
Iago Nunes <155097663+IagoDSN@users.noreply.github.com>
2025-05-21 11:25:31 -0300

    Update Teste.java

--- RevisaoIagoNunesINFOE/src/Atividade01/Teste.java
+++ RevisaoIagoNunesINFOE/src/Atividade01/Teste.java
@@ -5,12 +5,18 @@
  */
 package Atividade01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 13410289682
  */
 public class Teste {
     public static void main(String[] args) {
+       Secretaria secretaria = new Secretaria();
+       Scanner entrada = new Scanner(System.in);
+       
+        System.out.println("D");
        
     }
 }
