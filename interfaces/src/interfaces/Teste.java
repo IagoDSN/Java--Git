1cf2c9c3613925de9c4bda235ba03d3f955edbfb
Iago Nunes <155097663+IagoDSN@users.noreply.github.com>
2025-07-09 11:18:40 -0300

    interface

--- interfaces/src/interfaces/Teste.java
+++ interfaces/src/interfaces/Teste.java
@@ -12,9 +12,12 @@ package interfaces;
 public class Teste {
 public static void main(String[] args) {
 AreaCalculavel a = new Retangulo(3,2);
-System.out.println("Quadrado: "+a.calculaArea());
+System.out.println("Retangulo: "+a.calculaArea());
 
 AreaCalculavel c = new Circulo(3);
 System.out.println("Circulo: "+c.calculaArea());
+
+AreaCalculavel q = new Quadrado(4);
+System.out.println("Quadrado: "+q.calculaArea());
 }
 }
